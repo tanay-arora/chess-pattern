3a113e5608c81ff7657b4a8212da726ae88d0075
Tanay <72656007+tanayluthra001@users.noreply.github.com>
2020-11-07 18:03:51 +0530

    Update chess-pattren1.sh

--- chess-pattren1.sh
+++ chess-pattren1.sh
@@ -1,9 +1,9 @@
 echo "enter any number"
 read input;
 echo ""
-for (( x=2;x<$input+2;x++ ));
+for (( x=2;x<=$input+1;x++ ));
 do 
-for (( y=2;y<$input+2;y++ ));
+for (( y=2;y<=$input+1;y++ ));
 do
 (( i = $x%2 ))
 (( j = $y%2 ))
