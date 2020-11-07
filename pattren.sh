dc049779474ad62a32e8323dc676350caf611786
Tanay <72656007+tanayluthra001@users.noreply.github.com>
2020-11-07 17:44:10 +0530

    Add files via upload
    this file is also work the same but with less code.

---
+++ pattren.sh
@@ -0,0 +1,18 @@
+echo "enter any number"
+read input;
+echo ""
+for (( x=2;x<$input+2;x++ ));
+do 
+for (( y=2;y<$input+2;y++ ));
+do
+(( i = $x%2 ))
+(( j = $y%2 ))
+if [[ $i == 0 && $j == 1 || $i == 1 && $j == 0 ]]
+then
+printf " 0 "
+else
+printf " 1 ";
+fi
+done
+echo " "
+done
